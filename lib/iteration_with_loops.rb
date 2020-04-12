21d4d73b9972ced9e706c70fe5bcb19e066ebbd1
Steven Ross <ross.steven@me.com>
2020-04-12 15:03:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,7 @@ min_in_nested_arrays = []
 while row_index < src.length do
   element_index = 0
   inner_values = []
+  minimum_temp = 100
   while element_index < src[row_index].length do
     if src[row_index][element_index] < minimum_temp
       minimum_temp = src[row_index][element_index]
